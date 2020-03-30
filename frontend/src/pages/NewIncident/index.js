30af8fa569a23685487409d4488df34098b160a8
Jonata Serpa <jonataserpa@gmail.com>
2020-03-29 23:24:58 -0300

    useHistory

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import api from '../../services/api';
 import './styles.css';
