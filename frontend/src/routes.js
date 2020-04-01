db968a5080c796bdb546d61ad09100831c180284
Jonata Serpa <jonataserpa@gmail.com>
2020-03-31 23:09:12 -0300

    Front-end da Aplicação

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,11 +6,13 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NewIncident from './pages/NewIncident';
 
+/* routes */
+
 export default function Routes (){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Logon} />
+                <Route path="/" exact component={Logon} /> 
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/incidents/new" component={NewIncident} />
