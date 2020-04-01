d078665c7c3b20614adfaa3434705ac22bce59f1
Jonata Serpa <jonataserpa@gmail.com>
2020-03-31 23:05:52 -0300

    Back-end da Aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
-app.use(cors()); // permissao da onde tem acesso, { origin : 'https://meusite.com' }
+app.use(cors()); // permissão da onde tem acesso, { origin : 'https://meusite.com' }
 app.use(express.json()); // para o express entender as requisições no formato Json no corpo
 app.use(routes);
 app.listen(3333);
